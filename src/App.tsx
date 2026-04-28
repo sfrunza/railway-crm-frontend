f42b8e7117d6a2646cd1d94a9b37f5f79b5e776f
sfrunza <frunza.sergiu3@gmail.com>
2026-04-27 21:14:10 -0400

    api setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import { Demo } from './components/demo';
 
+interface Response {
+  message: string;
+}
+
 export default function App() {
-  return <Demo />;
+  const { data, isLoading, error } = useQuery<Response>({
+    queryKey: ['demo'],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/posts`).then((res) => res.json()),
+  });
+
+  console.log(data);
+
+  return (
+    <>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {data && <div>Data: {data.message}</div>}
+      <Demo />
+    </>
+  );
 }
