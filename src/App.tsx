8c766cdb848a36f32d3d11fdf5b0bebf5f980d17
sfrunza <frunza.sergiu3@gmail.com>
2026-04-27 21:19:32 -0400

    api

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ interface Response {
   message: string;
 }
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function App() {
   const { data, isLoading, error } = useQuery<Response>({
     queryKey: ['demo'],
-    queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/posts`).then((res) => res.json()),
+    queryFn: () => fetch(`${API_URL}/posts`).then((res) => res.json()),
   });
 
   console.log(data);
