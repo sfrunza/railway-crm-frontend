43d7a4743c682de15dc8c85ec525b5eb403040a6
sfrunza <frunza.sergiu3@gmail.com>
2026-04-27 21:22:21 -0400

    api fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default function App() {
   const { data, isLoading, error } = useQuery<Response>({
     queryKey: ['demo'],
-    queryFn: () => fetch(`${API_URL}/posts`).then((res) => res.json()),
+    queryFn: () => fetch(`${API_URL}/api/v1/posts`).then((res) => res.json()),
   });
 
   console.log(data);
